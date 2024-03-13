e54174ff048ef4d7219af0e46801c729e582ebef
kojimakojima <koko1217.kojima@gmail.com>
2024-03-13 19:36:06 +0100

    check Blackjack

---
+++ src/helper/checkBlackjack.ts
@@ -0,0 +1,20 @@
+export default function checkBlackjack(cards: Array<CardType>): boolean {
+  let ace = 0;
+  let face = 0;
+
+  for (const e of cards) {
+    switch (e.value) {
+      case "ACE":
+        ace++;
+        break;
+      case "JACK":
+      case "QUEEN":
+      case "KING":
+        face++;
+      default:
+        break;
+    }
+  }
+  if (cards.length === 2 && ace === 1 && face === 1) return true;
+  else return false;
+}
