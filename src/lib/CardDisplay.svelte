fbf67ea488ce742cae772467886b2ad3b730a9ce
kojimakojima <koko1217.kojima@gmail.com>
2024-03-16 15:25:56 +0100

    added dealer message

--- src/lib/CardDisplay.svelte
+++ src/lib/CardDisplay.svelte
@@ -1,7 +1,17 @@
 <script lang="ts">
+  export let isDealerMessage: boolean = false;
   export let dealerAvatar: string = "";
   export let dealerCards: Array<CardType> = [];
   export let playerCards: Array<CardType> = [];
+
+  let isHoveredMessage = false;
+
+  function handleMouseEnterMessage() {
+    isHoveredMessage = true;
+  }
+  function handleMouseLeaveMessage() {
+    isHoveredMessage = false;
+  }
 </script>
 
 <div>
@@ -9,9 +19,24 @@
     <img
       src={dealerAvatar}
       alt="Dealer's Avatar"
-      class="w-36 transition-transform hover:scale-105"
+      class="h-48 transition-transform hover:scale-105"
     />
   </p>
+  {#if isDealerMessage}
+    <p
+      on:mouseenter={handleMouseEnterMessage}
+      on:mouseleave={handleMouseLeaveMessage}
+      class="w-80 border-2 bg-zinc-900 p-4 text-center font-bold md:w-96"
+    >
+      {#if isHoveredMessage}
+        <span class="text-sm md:text-base">
+          Ten years early to beat me 10 times in a row.
+        </span>
+      {:else}
+        俺に十連勝するなんて十年早いんだよ。
+      {/if}
+    </p>
+  {/if}
   <!-- Dealer Cards -->
   <div class="mb-8 flex justify-center gap-2">
     {#each dealerCards as card, i}
